d8940db7904a1e89427e09f1e1673a4c5b50d7cd
mevlutardic <mevlutardic.1@hotmail.com>
2024-07-07 23:50:29 +0300

    added information

--- remover.py
+++ remover.py
@@ -14,7 +14,7 @@ class GrabCut(threading.Thread):
         self.kSize = kSize
         self.i = 0
         self.sourcelist = os.listdir(self.filepath)
-
+        self.informations()
         cv2.namedWindow(self.windowName, cv2.WINDOW_AUTOSIZE)
         cv2.namedWindow('output', cv2.WINDOW_AUTOSIZE)
 
@@ -31,7 +31,7 @@ class GrabCut(threading.Thread):
 
         self.mask = np.zeros(self.image_new.shape[:2], dtype=np.uint8)
         
-        self.mask2 = None
+        self.grapcut_mask = None
 
         self.output = np.zeros(self.image_new.shape, np.uint8)
 
@@ -64,14 +64,24 @@ class GrabCut(threading.Thread):
         
     def informations(self):
         print("""
-        Press 'q' to exit the loop
-        Press 'n' to go to the next photo
-        Press 'b' to go to the previous photo
-        Press 'm' to toggle mode (e.g., exit drawing mode)
-        Press 'c' to enable circle drawing mode
-        Press 'g' to run the GrabCut algorithm
-        
-        """)
+                Keyboard Controls:
+                ------------------
+                Press 'q' : Exit the loop
+                Press 'n' : Go to the next photo
+                Press 'b' : Go to the previous photo
+                Press 'm' : Toggle mode (e.g., exit drawing mode)
+                Press 'c' : Enable circle drawing mode
+                Press 'g' : Run the GrabCut algorithm
+                Press 'r' : Remove parts of the image
+                
+                Drawing Modes:
+                --------------
+                Press '0' : Draw background (BG)
+                Press '1' : Draw foreground (FG)
+                Press '2' : Draw probable background (PR_BG)
+                Press '3' : Draw probable foreground (PR_FG)
+            """)
+
 
     def run(self):
         while True:
@@ -83,50 +93,50 @@ class GrabCut(threading.Thread):
             code = cv2.waitKey(1)
 
             if code == ord("q"):  # Press 'q' to exit the loop
-                print("Exiting the program...")
+                print(f'\r --- Exiting the program...\n\n                                   ', end='', flush=True)
                 break
             elif code == ord("n"):  # Press 'n' to go to the next photo
                 if self.i < len(self.sourcelist) - 1:
                     self.i += 1
                     self.update_photo(self.i)
-                    print("Moving to the next photo...")
+                    print(f'\r --- Moving to the next photo...                              ', end='', flush=True)
             elif code == ord("b"):  # Press 'b' to go to the previous photo
                 if self.i > 0:
                     self.i -= 1
                     self.update_photo(self.i)
-                    print("Moving to the previous photo...")
+                    print(f'\r --- Moving to the previous photo...                          ', end='', flush=True)
             elif code == ord("m"):  # Press 'm' to toggle mode (e.g., exit drawing mode)
                 self.mode = not self.mode
                 self.draw_circle = False
-                print("Toggling mode...")
+                print(f'\r --- Toggling mode...                                     ', end='', flush=True)
             elif code == ord("c"):  # Press 'c' to enable circle drawing mode
                 self.draw_circle = True
-                print("Circle drawing mode enabled...")
+                print(f'\r -- Enable circle drawing mode...                         ', end='', flush=True)
             elif code == ord("g"):  # Press 'g' to run the GrabCut algorithm
                 self.grab_cut()
-                print("Running GrabCut algorithm...")
+                print(f'\r --- Running GrabCut algorithm...                         ', end='', flush=True)
             elif code == ord("r"):  # Press 'r' to remove parts of the image
                 kernel = np.ones((5,5), np.uint8)
-                self.mask2 = cv2.dilate(self.mask2, kernel, iterations=1)
-                dst = cv2.inpaint(self.image_new, self.mask2, 3, cv2.INPAINT_NS)
-                filled_image = inpaint.inpaint_biharmonic(self.image_new, self.mask2, split_into_regions=True, channel_axis=-1)
-                filled_image2 = inpaint.inpaint_biharmonic(dst, self.mask2, split_into_regions=True, channel_axis=-1)
+                self.grapcut_mask = cv2.dilate(self.grapcut_mask, kernel, iterations=1)
+                dst = cv2.inpaint(self.image_new, self.grapcut_mask, 3, cv2.INPAINT_NS)
+                filled_image = inpaint.inpaint_biharmonic(self.image_new, self.grapcut_mask, split_into_regions=True, channel_axis=-1)
+                filled_image2 = inpaint.inpaint_biharmonic(dst, self.grapcut_mask, split_into_regions=True, channel_axis=-1)
                 cv2.imshow("out", dst)
                 cv2.imshow("filled_image", filled_image)
                 cv2.imshow("filled_image2", filled_image2)
-                print("Removing parts of the image and displaying results...")
+                print(f'\r -- Removing parts of the image and displaying results... ', end='', flush=True)
             elif code == ord('0'):  # Press '0' for background (BG) drawing
-                self.value = self.DRAW_BG
-                print("Background drawing selected...")
+                self.value = self.DRAW_BG 
+                print(f'\r -- Background drawing selected...                        ', end='', flush=True)
             elif code == ord('1'):  # Press '1' for foreground (FG) drawing
                 self.value = self.DRAW_FG
-                print("Foreground drawing selected...")
+                print(f'\r -- Foreground drawing selected...                        ', end='', flush=True)
             elif code == ord('2'):  # Press '2' for probable background (PR_BG) drawing
                 self.value = self.DRAW_PR_BG
-                print("Probable background drawing selected...")
+                print(f'\r -- Probable background drawing selected...               ', end='', flush=True)
             elif code == ord('3'):  # Press '3' for probable foreground (PR_FG) drawing
                 self.value = self.DRAW_PR_FG
-                print("Probable foreground drawing selected...")
+                print(f'\r -- Probable foreground drawing selected...               ', end='', flush=True)
 
 
         cv2.destroyAllWindows()
@@ -144,14 +154,14 @@ class GrabCut(threading.Thread):
             self.img = cv2.cvtColor(self.image_new, cv2.COLOR_RGBA2RGB)
             cv2.grabCut(self.img, self.mask, self.rect, bgdmodel, fgdmodel, 7, cv2.GC_INIT_WITH_MASK)
         
-        self.mask2 = np.where((self.mask == 1) + (self.mask == 3), 255, 0).astype('uint8')
+        self.grapcut_mask = np.where((self.mask == 1) + (self.mask == 3), 255, 0).astype('uint8')
         
-        if np.any(self.mask2): 
+        if np.any(self.grapcut_mask): 
             
-            self.output = cv2.bitwise_and(self.img, self.img, mask=self.mask2)
+            self.output = cv2.bitwise_and(self.img, self.img, mask=self.grapcut_mask)
            
         else:
-            print("mask2 is empty.")
+            print("grapcut_mask is empty.")
 
     def update_photo(self, i):
         path = os.path.join(self.filepath, self.sourcelist[i])
@@ -168,16 +178,16 @@ class GrabCut(threading.Thread):
                 self.start_point_rect = (x, y)
             elif event == cv2.EVENT_MOUSEMOVE:
                 if self.rectangle:
-                    self.image2 = self.image_new.copy()
+                    self.imaga_drawing = self.image_new.copy()
                     ix = self.start_point_rect[0]
                     iy = self.start_point_rect[1]
-                    overlay = self.image2.copy() 
-                    cv2.rectangle(self.image2, (ix, iy), (x, y), self.BLUE, 3)
+                    overlay = self.imaga_drawing.copy() 
+                    cv2.rectangle(self.imaga_drawing, (ix, iy), (x, y), self.BLUE, 3)
                     alpha = 0.6
-                    self.image2 = cv2.addWeighted(overlay, alpha, self.image2, 1 - alpha, 0) 
+                    self.imaga_drawing = cv2.addWeighted(overlay, alpha, self.imaga_drawing, 1 - alpha, 0) 
                     self.rect = (min(ix, x), min(iy, y), abs(ix - x), abs(iy - y))
                     self.rect_or_mask = 0
-                    cv2.imshow(self.windowName, self.image2)
+                    cv2.imshow(self.windowName, self.imaga_drawing)
             elif event == cv2.EVENT_LBUTTONUP:
                 self.rectangle = False
                 self.rect_over = True
@@ -189,17 +199,17 @@ class GrabCut(threading.Thread):
         elif self.rect_over and self.draw_circle:
             if event == cv2.EVENT_LBUTTONDOWN:
                 self.drawing = True
-                cv2.circle(self.image2, (x, y), self.thickness, self.value['color'], -1)
+                cv2.circle(self.imaga_drawing, (x, y), self.thickness, self.value['color'], -1)
                 cv2.circle(self.mask, (x, y), self.thickness, self.value['val'], -1)
             elif event == cv2.EVENT_MOUSEMOVE:
                 if self.drawing:
-                    cv2.circle(self.image2, (x, y), self.thickness, self.value['color'], -1)
+                    cv2.circle(self.imaga_drawing, (x, y), self.thickness, self.value['color'], -1)
                     cv2.circle(self.mask, (x, y), self.thickness, self.value['val'], -1)
-                    cv2.imshow(self.windowName, self.image2)
+                    cv2.imshow(self.windowName, self.imaga_drawing)
             elif event == cv2.EVENT_LBUTTONUP:
                 if self.drawing:
                     self.drawing = False
-                    cv2.circle(self.image2, (x, y), self.thickness, self.value['color'], -1)
+                    cv2.circle(self.imaga_drawing, (x, y), self.thickness, self.value['color'], -1)
                     cv2.circle(self.mask, (x, y), self.thickness, self.value['val'], -1)
 
 if __name__ == '__main__':
